1a445aaced620cb06b9bb9fe24dd3b22b700adf5
dnemirich <dnemirich@gmail.com>
2024-04-09 20:59:06 +0200

    feat: task 3.1 add

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,7 +1,7 @@
 const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
 
 function createColorString() {
-    return colors.filter(color => ["черный", "красный", "желтый"].includes(color)).join('-');
+    return colors.filter(color => color === "черный" || color === "красный" || color === "желтый").join('-');
 }
 
 console.log(createColorString());
